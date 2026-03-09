8a30ce3b08dd5eeea5018344b656a9bb90ec60be
NewAwesome <nh13573019973@163.com>
2026-03-09 09:01:13 +0800

    fix some potential bugs

--- test/test_coordiate_transformation.py
+++ test/test_coordiate_transformation.py
@@ -30,7 +30,28 @@ def test_rotmat_to_T() -> None:
 def test_transform() -> None:
     transf = Coordinate_Transformation()
     transf.parse_config(str(Path(CONFIG_PATH, "Config.json")), str(Path(CONFIG_PATH, "install_markers.json")))
+    euler = [180.0, 0.0, 90]
+    t_vec = [1.1, 1.0, 10.0]
+    rot_mat = euler_to_rotmat(euler, degree=True)
+    input_seg = {"id": 0,
+                 "rot_mat": rot_mat,
+                 "t_vec": t_vec,
+                 "error": 0.325325}
+    output_seg = transf.transform(input_seg, 0)
+    print(output_seg)
+    print(rotmat_to_euler(output_seg["rot_mat"], degree=True))
+    euler2 = [180.0, 0, 0]
+    t_vec2 = [0, 0, 40]
+    rot_mat2 = euler_to_rotmat(euler2, degree=True)
+    input_seg2 = {"id": 1,
+                  "rot_mat": rot_mat2,
+                  "t_vec": t_vec2,
+                  "error": 0.325325}
+    output_seg2 = transf.transform(input_seg2, 1)
+    print(output_seg2)
+    print(rotmat_to_euler(output_seg2["rot_mat"], degree=True))
+    
 
 
 if __name__ == "__main__":
-    test_rotmat_to_T()
+    test_transform()
